f3ad34dc8186f9633132bf6a9b3ed576bdf63e6e
suecharo <suehiro619@gmail.com>
2021-01-05 16:37:51 +0900

    Remove unused library

--- app/src/plugins/vuetifyColors.ts
+++ app/src/plugins/vuetifyColors.ts
@@ -1,13 +1,6 @@
 import { Context } from '@nuxt/types'
 import { Inject } from '@nuxt/types/app'
 import colors from 'vuetify/lib/util/colors'
-import Colors from 'vuetify/types'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $colors: Colors
-  }
-}
 
 export default (_context: Context, inject: Inject): void => {
   inject('colors', colors)
