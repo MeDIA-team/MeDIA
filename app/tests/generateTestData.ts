6ec7ccf98a68dc0598ef07da88889865116afc8c
suecharo <suehiro619@gmail.com>
2021-01-12 18:10:16 +0900

    Update generating test data script

--- app/tests/generateTestData.ts
+++ app/tests/generateTestData.ts
@@ -105,6 +105,29 @@ const getAge = (from: Date, to: Date) => {
   return dayjs(to).diff(dayjs(from), 'year')
 }
 
+type Project = {
+  projectID: string
+  projectName: string
+}
+
+type DataType = {
+  name: string
+  [K: string]: string | number
+}
+
+const defaultFields: string[] = [
+  'projectID',
+  'projectName',
+  'projectPatientID',
+  'patientID',
+  'sex',
+  'age',
+  'disease',
+  'sampleID',
+  'samplingDate',
+  'dataType',
+]
+
 type Entry = {
   projectID: string
   projectName: string
@@ -121,18 +144,12 @@ type Entry = {
 
 type Patient = {
   patientID: string
-  projects: {
-    projectID: string
-    projectName: string
-  }[]
+  projects: Project[]
   projectPatientIDs: string[]
   samples: {
     sampleID: string
     samplingDate: string
-    dataTypes: {
-      name: string
-      [K: string]: string | number
-    }[]
+    dataTypes: DataType[]
     sex: string
     age: number
     disease: string
@@ -142,35 +159,86 @@ type Patient = {
 type Sample = {
   sampleID: string
   samplingDate: string
-  dataTypes: {
-    name: string
-    [K: string]: string | number
-  }[]
+  dataTypes: DataType[]
   patientID: string
   sex: string
   age: number
   disease: string
-  projects: {
-    projectID: string
-    projectName: string
-  }[]
+  projects: Project[]
   projectPatientIDs: string[]
+}
+
+const mergeProject = (projectArr: Project[], project: Project) => {
+  const projectNames = projectArr.map((p) => p.projectID)
+  if (!projectNames.includes(project.projectID)) {
+    return [...projectArr, project]
+  }
+  return projectArr
+}
+
+const mergeStringArr = (arr: string[], val: string) => {
+  if (!arr.includes(val)) {
+    return [...arr, val]
+  }
+  return arr
+}
+
+const mergeDataType = (dataTypeArr: DataType[], entry: Entry) => {
+  const dataType = entry.dataType
+  const dataTypeNames = dataTypeArr.map((d) => d.name)
+  if (!dataTypeNames.includes(dataType)) {
+    dataTypeArr.push({ name: dataType })
+  }
+  for (const [key, val] of Object.entries(entry)) {
+    if (!defaultFields.includes(key)) {
+      for (let i = 0; i < dataTypeArr.length; i++) {
+        if (dataType === dataTypeArr[i].name) {
+          dataTypeArr[i][key] = val
+        }
+      }
+    }
+  }
+  return dataTypeArr
+}
+
+const mergeSample = (
+  sampleArr: {
+    sampleID: string
+    samplingDate: string
+    dataTypes: DataType[]
+    sex: string
+    age: number
+    disease: string
+  }[],
+  entry: Entry
+) => {
+  const sampleID = entry.sampleID
+  const sampleIDs = sampleArr.map((s) => s.sampleID)
+  if (!sampleIDs.includes(sampleID)) {
+    sampleArr.push({
+      sampleID: entry.sampleID,
+      samplingDate: entry.samplingDate,
+      dataTypes: [],
+      sex: entry.sex,
+      age: entry.age,
+      disease: entry.disease,
+    })
+  }
+  for (let i = 0; i < sampleArr.length; i++) {
+    if (sampleArr[i].sampleID === sampleID) {
+      sampleArr[i].dataTypes = mergeDataType(sampleArr[i].dataTypes, entry)
+    }
+  }
+  return sampleArr
 }
 
 const generateTestData = (
   patientNum: number
 ): { entries: Entry[]; patients: Patient[]; samples: Sample[] } => {
-  const patients = generatePatients(patientNum)
-
   const entriesData: Entry[] = []
-  const patientsData: Patient[] = []
-  const samplesData: Sample[] = []
-
+  const patients = generatePatients(patientNum)
   for (const patient of patients) {
-    const projects: {
-      projectName: string
-      projectID: string
-    }[] = []
+    const projects: Project[] = []
     const projectNum = randomInt(1, PROJECTS.length)
     while (projects.length < projectNum) {
       const project = randomChoice(PROJECTS)
@@ -182,35 +250,24 @@ const generateTestData = (
         projects.push(project)
       }
     }
-    const projectPatientIDs = projects.map(
-      (project) => `${project.projectID}_${patient.patientID}`
-    )
+    for (const project of projects) {
+      const sampleNum = randomInt(1, 5)
+      for (let i = 0; i < sampleNum; i++) {
+        const sampleId = generateRandomId(32)
+        const samplingDate = generateRandomDate(
+          new Date(2010, 1, 1),
+          new Date(2019, 12, 31)
+        )
 
-    const patientData: Patient = {
-      patientID: patient.patientID,
-      projects,
-      projectPatientIDs,
-      samples: [],
-    }
-
-    const sampleNum = randomInt(1, 5)
-    for (let i = 0; i < sampleNum; i++) {
-      const sampleId = generateRandomId(32)
-      const samplingDate = generateRandomDate(
-        new Date(2010, 1, 1),
-        new Date(2019, 12, 31)
-      )
-
-      const dataTypes: string[] = []
-      const dataNum = randomInt(1, DATA_TYPE.length)
-      while (dataTypes.length < dataNum) {
-        const dataType = randomChoice(DATA_TYPE)
-        if (!dataTypes.includes(dataType)) {
-          dataTypes.push(dataType)
+        const dataTypes: string[] = []
+        const dataNum = randomInt(1, DATA_TYPE.length)
+        while (dataTypes.length < dataNum) {
+          const dataType = randomChoice(DATA_TYPE)
+          if (!dataTypes.includes(dataType)) {
+            dataTypes.push(dataType)
+          }
         }
-      }
-      for (const dataType of dataTypes) {
-        for (const project of projects) {
+        for (const dataType of dataTypes) {
           const entry: Entry = {
             projectID: project.projectID,
             projectName: project.projectName,
@@ -232,56 +289,64 @@ const generateTestData = (
           entriesData.push(entry)
         }
       }
-      const ps = {
-        sampleID: sampleId,
-        samplingDate: dayjs(samplingDate).format('YYYY-MM-DD'),
-        dataTypes: [] as {
-          name: string
-          [K: string]: string | number
-        }[],
-        sex: patient.sex,
-        age: getAge(patient.birthDate, samplingDate),
-        disease: patient.disease,
-      }
-      const s: Sample = {
-        sampleID: sampleId,
-        samplingDate: dayjs(samplingDate).format('YYYY-MM-DD'),
-        dataTypes: [] as {
-          name: string
-          [K: string]: string | number
-        }[],
-        patientID: patient.patientID,
-        sex: patient.sex,
-        age: getAge(patient.birthDate, samplingDate),
-        disease: patient.disease,
-        projects,
-        projectPatientIDs,
-      }
-      for (const dataType of dataTypes) {
-        const d: {
-          name: string
-          [K: string]: string | number
-        } = {
-          name: dataType,
-        }
-        if (dataType === 'RNAseq') {
-          d['Library Prep'] = randomChoice(LIBRARY_PREP)
-        } else if (dataType === 'Skin image') {
-          d['Body Region'] = randomChoice(BODY_REGION)
-        }
-        ps.dataTypes.push(d)
-        s.dataTypes.push(d)
-      }
-      patientData.samples.push(ps)
-      samplesData.push(s)
     }
-    patientsData.push(patientData)
+  }
+
+  const samplesData: Record<string, Sample> = {}
+  const patientsData: Record<string, Patient> = {}
+  for (const entry of entriesData) {
+    if (!(entry.sampleID in samplesData)) {
+      samplesData[entry.sampleID] = {
+        sampleID: entry.sampleID,
+        samplingDate: entry.samplingDate,
+        dataTypes: [],
+        patientID: entry.patientID,
+        sex: entry.sex,
+        age: entry.age,
+        disease: entry.disease,
+        projects: [],
+        projectPatientIDs: [],
+      }
+    }
+    samplesData[entry.sampleID].dataTypes = mergeDataType(
+      samplesData[entry.sampleID].dataTypes,
+      entry
+    )
+    samplesData[entry.sampleID].projects = mergeProject(
+      samplesData[entry.sampleID].projects,
+      { projectName: entry.projectName, projectID: entry.projectID }
+    )
+    samplesData[entry.sampleID].projectPatientIDs = mergeStringArr(
+      samplesData[entry.sampleID].projectPatientIDs,
+      entry.projectPatientID
+    )
+
+    if (!(entry.patientID in patientsData)) {
+      patientsData[entry.patientID] = {
+        patientID: entry.patientID,
+        projects: [],
+        projectPatientIDs: [],
+        samples: [],
+      }
+    }
+    patientsData[entry.patientID].projects = mergeProject(
+      patientsData[entry.patientID].projects,
+      { projectName: entry.projectName, projectID: entry.projectID }
+    )
+    patientsData[entry.patientID].projectPatientIDs = mergeStringArr(
+      patientsData[entry.patientID].projectPatientIDs,
+      entry.projectPatientID
+    )
+    patientsData[entry.patientID].samples = mergeSample(
+      patientsData[entry.patientID].samples,
+      entry
+    )
   }
 
   return {
     entries: entriesData,
-    patients: patientsData,
-    samples: samplesData,
+    patients: Object.values(patientsData),
+    samples: Object.values(samplesData),
   }
 }
 
