549ac4ce54a16df9b066bb6eb5ffd6ff6641fbac
Tazro Inutano Ohta <inutano@gmail.com>
2021-03-17 14:54:59 +0900

    fix script

--- bin/save_docker_image.sh
+++ bin/save_docker_image.sh
@@ -11,9 +11,14 @@ if [[ -z $(which gzip) ]]; then
 fi
 
 BINDIR="$(cd $(dirname ${0}) && pwd -P)"
-BASEDIR="$(dirname ${BINDIR} && pwd -P)"
+BASEDIR="$(dirname ${BINDIR})"
 
 ENVFILE="${BASEDIR}/.env"
+if [[ ! -e ${ENVFILE} ]]; then
+  echo "ERROR: .env file not found" 1>&2
+  exit 1
+fi
+
 IMAGE_TAG=$(cat ${ENVFILE} | grep 'MEDIA_DOCKER_IMAGE_TAG' | cut -d '=' -f 2)
 CONTAINER_IMAGE_ID="ghcr.io/media-team/media_app:${IMAGE_TAG}"
 
@@ -27,17 +32,15 @@ docker pull ${CONTAINER_IMAGE_ID}
 echo "### Saving docker container image ${CONTAINER_IMAGE_ID} ..."
 docker save ${CONTAINER_IMAGE_ID} > ${IMAGE_OUT_PATH}
 
-if [[ -e ${IMAGE_OUT_PATH} ]]; then
-  echo "### Successfully saved at ${IMAGE_OUT_PATH}"
-
-  echo "### Removing cached image file ..."
-  docker rmi ${CONTAINER_IMAGE_ID}
-
-  echo "### Successfully finished!"
-  echo "### Copy .tar file to deployment environment and execute 'docker load -i $(basename ${IMAGE_OUT_PATH})' to load the image."
-
-  exit 0
-else
+if [[ ! -e ${IMAGE_OUT_PATH} ]]; then
   echo "ERROR: Failed to create container image"
   exit 1
 fi
+
+echo "### Image successfully saved at ${IMAGE_OUT_PATH}"
+
+echo "### Removing cached image file ..."
+docker rmi ${CONTAINER_IMAGE_ID}
+
+echo "### Process successfully finished!"
+echo "### Copy .tar file to deployment environment and execute 'docker load -i $(basename ${IMAGE_OUT_PATH})' to load the image."
