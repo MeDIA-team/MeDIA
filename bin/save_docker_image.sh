f2001313b9afc72d28f6fcafec8e14b8a2412377
Tazro Inutano Ohta <inutano@gmail.com>
2021-03-30 17:41:49 +0900

    save both image, compress in gzip

--- bin/save_docker_image.sh
+++ bin/save_docker_image.sh
@@ -13,34 +13,68 @@ fi
 BINDIR="$(cd $(dirname ${0}) && pwd -P)"
 BASEDIR="$(dirname ${BINDIR})"
 
+IMAGE_DIR="${BASEDIR}/container_images"
+mkdir -p ${IMAGE_DIR}
+
 ENVFILE="${BASEDIR}/.env"
 if [[ ! -e ${ENVFILE} ]]; then
   echo "ERROR: .env file not found" 1>&2
   exit 1
 fi
 
-IMAGE_TAG=$(grep 'MEDIA_DOCKER_IMAGE_TAG' ${ENVFILE} | cut -d '=' -f 2)
-CONTAINER_IMAGE_ID="ghcr.io/media-team/media_app:${IMAGE_TAG}"
+DOCKER_COMPOSE_FILE="${BASEDIR}/docker-compose.yml"
 
-IMAGE_DIR="${BASEDIR}/container_images"
-mkdir -p ${IMAGE_DIR}
-IMAGE_OUT_PATH="${IMAGE_DIR}/media_app_${IMAGE_TAG}.tar"
+get_image_tag() {
+  local image_name=${1}
+  case ${image_name} in
+    "app" )
+      grep 'MEDIA_DOCKER_IMAGE_TAG' ${ENVFILE} | cut -d '=' -f 2
+      ;;
+    "elasticsearch" )
+      grep 'docker.elastic.co' ${DOCKER_COMPOSE_FILE} | awk -F':' '$0=$3'
+      ;;
+  esac
+}
 
-echo "### Pulling docker container image from GitHub Container Registry ..."
-docker pull ${CONTAINER_IMAGE_ID}
+get_image_id() {
+  local image_name=${1}
+  local image_tag=${2}
+  case ${image_name} in
+    "app" )
+      echo "ghcr.io/media-team/media_app:${image_tag}"
+      ;;
+    "elasticsearch" )
+      grep 'docker.elastic.co' ${DOCKER_COMPOSE_FILE} | awk '$0=$2'
+      ;;
+  esac
+}
 
-echo "### Saving docker container image ${CONTAINER_IMAGE_ID} ..."
-docker save ${CONTAINER_IMAGE_ID} > ${IMAGE_OUT_PATH}
+for image_name in "app" "elasticsearch"; do
+  image_tag=$(get_image_tag "${image_name}")
+  container_image_id=$(get_image_id "${image_name}" "${image_tag}")
+  image_out_path="${IMAGE_DIR}/media_${image_name}_${image_tag}.tar.gz"
 
-if [[ ! -e ${IMAGE_OUT_PATH} ]]; then
-  echo "ERROR: Failed to create container image"
-  exit 1
-fi
+  echo "### Pulling docker container image for ${image_name} ..."
+  docker pull ${container_image_id}
+  echo "### done."
 
-echo "### Image successfully saved at ${IMAGE_OUT_PATH}"
+  echo "### Saving docker container image ${container_image_id} ..."
+  docker save ${container_image_id} | gzip > ${image_out_path}
+  echo "### done."
 
-echo "### Removing cached image file ..."
-docker rmi ${CONTAINER_IMAGE_ID}
+  if [[ ! -e ${image_out_path} ]]; then
+    echo "ERROR: Failed to create container image ${container_image_id}"
+    exit 1
+  fi
+
+  echo "### Image successfully saved at ${image_out_path}"
+
+  echo "### Removing cached image file ..."
+  docker rmi ${container_image_id}
+  echo "### done."
+
+  echo "### Copy .tar file to deployment environment and execute 'docker load < $(basename ${image_out_path})' to load the image."
+  echo "### Saving image for ${image_name} done."
+done
 
 echo "### Process successfully finished!"
-echo "### Copy .tar file to deployment environment and execute 'docker load -i $(basename ${IMAGE_OUT_PATH})' to load the image."
